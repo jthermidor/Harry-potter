1f93bb8df13665528c0ed235e422ca8020728274
jthermidor <43036743+jthermidor@users.noreply.github.com>
2019-03-25 08:59:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".wands1").click(function() {
+    $(".magic").slideToggle();
+});
+$(".wands").click(function() {
+    $(".img").fadeIn();
+});
+$(".wands2").click(function() {
+    $(".img2").slideToggle();
+});
